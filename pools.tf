1e67b4621cb8b72a1effa84599296f0c93ea3a0f
plemaist <plemaist@cisco.com>
2022-01-24 11:20:46 -0600

    added fc pool

--- pools.tf
+++ pools.tf
@@ -37,12 +37,12 @@ resource "intersight_fcpool_pool" "fcpool_pool1" {
   name = "${var.policy_prefix}-wwnn-pool"
   description = var.description
   assignment_order = "sequential"
-  pool_pool_purpose = "wwpn"
   id_blocks {
     object_type = "fcpool.Block"
     from        = "20:00:00:CA:FE:00:00:01"
     size          = "255"
     }
+  pool_pool_purpose = "wwpn"
   organization {
     object_type = "organization.Organization"
     moid = var.organization 
