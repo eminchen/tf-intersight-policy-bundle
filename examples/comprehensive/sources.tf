8a12991015d9277e1bdc4bfafc5830c52644b7d9
plemaist <plemaist@cisco.com>
2022-01-24 12:30:51 -0600

    cleaned up extra files

--- examples/comprehensive/sources.tf
+++
@@ -1,3 +0,0 @@
-data "intersight_organization_organization" "myorg" {
-  name = var.organization_name
-}
--- versions.tf
+++ versions.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     intersight = {
       source  = "CiscoDevNet/intersight"
-    #  version = ">=1.0.8"
       version = ">=1.0.20"
     }
   }
