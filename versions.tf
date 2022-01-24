7204ca82bf4fec2543ddb75b29dc48f1f48187b5
plemaist <plemaist@cisco.com>
2022-01-24 11:26:35 -0600

    added fc pool

--- versions.tf
+++ versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     intersight = {
       source  = "CiscoDevNet/intersight"
-      version = ">=1.0.8"
+    #  version = ">=1.0.8"
+      version = ">=1.0.21"
     }
   }
 }
