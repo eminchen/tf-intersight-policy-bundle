708cc7e0efe1ac76c26aea4c1c0e4eb7dc32368c
plemaist <plemaist@cisco.com>
2022-01-24 11:01:12 -0600

    added mac pool

--- pools.tf
+++ pools.tf
@@ -6,13 +6,17 @@ resource "intersight_ippool_pool" "ippool_pool1" {
     from = "1.1.1.101"
     size = "99"
   }
-
   ip_v4_config {
     object_type = "ippool.IpV4Config"
     gateway = "1.1.1.1"
     netmask = "255.255.255.0"
     primary_dns = "8.8.8.8"
     }
+  organization {
+      object_type = "organization.Organization"
+      moid = var.organization
+      }
+}
 
 resource "intersight_macpool_pool" "macpool_pool1" {
   name = "${var.policy_prefix}-mac-pool"
@@ -20,12 +24,9 @@ resource "intersight_macpool_pool" "macpool_pool1" {
     object_type = "macpool.Block"
     from        = "00:CA:FE:00:00:01"
     size          = "254"
-  }
-}
-
-organization {
-  object_type = "organization.Organization"
-  moid = var.organization
-  }
+    }
+  organization {
+    object_type = "organization.Organization"
+    moid = var.organization }
 }
  
