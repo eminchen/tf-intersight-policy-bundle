8cb19384ad221c312daad1ecbd7a79d06b1dfe14
plemaist <plemaist@cisco.com>
2022-01-24 12:26:07 -0600

    added fc pool

--- pools.tf
+++ pools.tf
@@ -33,13 +33,56 @@ resource "intersight_macpool_pool" "macpool_pool1" {
     }
 }
 
+resource "intersight_fcpool_pool" "fcpool_pool0" {
+  name = "${var.policy_prefix}-wwnn-pool"
+  description = var.description
+  assignment_order = "sequential"
+  pool_purpose = "WWNN"
+  id_blocks {
+    from        = "20:00:00:CA:FE:00:00:01"
+    size        =  255
+    }
+  organization {
+    object_type = "organization.Organization"
+    moid = var.organization 
+    }
+  dynamic "tags" {
+    for_each = var.tags
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+}
+
 resource "intersight_fcpool_pool" "fcpool_pool1" {
-  name = "${var.policy_prefix}-wwpn-pool"
+  name = "${var.policy_prefix}-wwpn-a-pool"
   description = var.description
   assignment_order = "sequential"
   pool_purpose = "WWPN"
   id_blocks {
     from        = "20:00:00:CA:FE:0A:00:01"
+    size        =  255
+    }
+  organization {
+    object_type = "organization.Organization"
+    moid = var.organization 
+    }
+  dynamic "tags" {
+    for_each = var.tags
+    content {
+      key   = tags.value.key
+      value = tags.value.value
+    }
+  }
+}
+resource "intersight_fcpool_pool" "fcpool_pool2" {
+  name = "${var.policy_prefix}-wwpn-b-pool"
+  description = var.description
+  assignment_order = "sequential"
+  pool_purpose = "WWPN"
+  id_blocks {
+    from        = "20:00:00:CA:FE:0B:00:01"
     size        =  255
     }
   organization {
